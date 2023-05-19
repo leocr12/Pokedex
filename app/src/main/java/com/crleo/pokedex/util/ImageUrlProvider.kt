6a624de3b74dfee5f8ed470b806f01ab1314f660
leocr12 <crleo012@gmail.com>
2023-05-19 18:52:49 -0400

    Remove image provider

--- app/src/main/java/com/crleo/pokedex/util/ImageUrlProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.crleo.pokedex.util
-
-object ImageUrlProvider {
-    private const val IMAGE_BASE_URL = "https://pokeapi.co/media/sprites/pokemon/"
-
-    fun getImageUrl(pokemonId: Int): String {
-        return "$IMAGE_BASE_URL.png"
-    }
-}
